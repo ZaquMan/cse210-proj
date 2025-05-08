165ab380257d6d799173c8cc3e7432fa09c91500
Zach Barnett <zacharman@live.com>
2025-05-08 15:55:43 -0700

    W01 E03 Main requirements

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Get user-generated magic number
+        //Console.Write("Please enter a number (DEBUG): ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        //int guess;
+        // Not sure if this needs to be declared before the do-while loop.
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (guess != magicNumber)
+
+        Console.WriteLine($"You got it! {magicNumber} is the magic number.");
     }
 }
