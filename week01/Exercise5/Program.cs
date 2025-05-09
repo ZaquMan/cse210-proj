3249be36674da9e7170664aad4cc99175b4b68f2
Zach Barnett <zacharybarnett96@gmail.com>
2025-05-08 19:46:01 -0700

    W01 E05

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,39 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userInput = Console.ReadLine();
+        return int.Parse(userInput);
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        // Forget saving the returns.  Just pass them immediately.
+        // If I had to reuse them this would be a problem, but here, it works.
+        DisplayResult(PromptUserName(), SquareNumber(PromptUserNumber()));
     }
 }
