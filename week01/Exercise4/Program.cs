467b54765ce33b01b294d7fc79ea03cabb415b9e
Zach Barnett <zacharybarnett96@gmail.com>
2025-05-08 19:24:25 -0700

    W01 E04 Main Requirements

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        string userInput = "0";
+        int userNum;
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            userInput = Console.ReadLine();
+            userNum = int.Parse(userInput);
+
+            if (userNum != 0)
+            {
+                numbers.Add(userNum);
+            }
+
+        } while (userInput != "0");
+
+        int sum = 0;
+        int largestNum = -99999;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+
+            if (number > largestNum)
+            {
+                largestNum = number;
+            }
+        }
+
+        float average = ((float)sum) / numbers.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNum}");
+
+
     }
 }
