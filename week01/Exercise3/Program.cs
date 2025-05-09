ff1ab7f0f3d15e356fd7468e925b58db7955b44c
Zach Barnett <zacharybarnett96@gmail.com>
2025-05-08 18:34:19 -0700

    W01 E03 Stretch Goals

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,31 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Get user-generated magic number
-        //Console.Write("Please enter a number (DEBUG): ");
-        //int magicNumber = int.Parse(Console.ReadLine());
+        bool replay = true;
 
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
-
-        //int guess;
-        // Not sure if this needs to be declared before the do-while loop.
-
-        do
+        while (replay)
         {
-            Console.Write("What is your guess? ");
-            int guess = int.Parse(Console.ReadLine());
+            // Get user-generated magic number
+            //Console.Write("Please enter a number (DEBUG): ");
+            //int magicNumber = int.Parse(Console.ReadLine());
 
-            if (guess > magicNumber)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("Higher");
-            }
-        } while (guess != magicNumber)
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
 
-        Console.WriteLine($"You got it! {magicNumber} is the magic number.");
+            //int guess;
+            // Not sure if this needs to be declared before the do-while loop.
+
+            int guess;
+            int numGuesses = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                numGuesses++;
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                }
+            } while (guess != magicNumber);
+
+            Console.WriteLine($"You got it! {magicNumber} is the magic number.  It took you {numGuesses} guesses.");
+
+
+            Console.Write("Would you like to play again? (Yes/No) ");
+            string replayString = Console.ReadLine();
+
+            if (Char.ToLower(replayString[0]) != 'y')
+            {
+                replay = false;
+            }
+        }
     }
 }
