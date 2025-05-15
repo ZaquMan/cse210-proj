00a8db0004ea8be8eba4eb07edd9fa3019f573bb
Zach Barnett <zacharybarnett96@gmail.com>
2025-05-14 21:49:52 -0700

    W02 Journal Stubs

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Entry
+{
+    string _date;
+    string _prompt;
+    string _response;
+
+    public void Display()
+    {
+        
+    }
+}
---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Journal
+{
+    List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile(string filename)
+    {
+
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        
+    }
+}
