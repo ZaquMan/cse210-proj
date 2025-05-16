65de0c4c8b9069bf19e879c67d02a5b4937eef9e
Zach Barnett <zacharybarnett96@gmail.com>
2025-05-15 20:29:35 -0700

    Added debug lines to stubs.  Got main program class mostly done.

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -6,21 +6,23 @@ public class Journal
 
     public void AddEntry(Entry newEntry)
     {
-
+        Console.WriteLine("DEBUG: New Entry HERE");
     }
 
     public void DisplayAll()
     {
-
+        // TODO: Display one message if the journal is empty.
+        // TODO: Iterate through entry list calling the .Display() func.
+        Console.WriteLine("DEBUG: Display Entries HERE");
     }
 
     public void SaveToFile(string filename)
     {
-
+        Console.WriteLine("DEBUG: Saving file, I guess");
     }
 
     public void LoadFromFile(string filename)
     {
-        
+        Console.WriteLine("DEBUG: Loading, Loading, Loading");
     }
 }
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -2,8 +2,66 @@ using System;
 
 class Program
 {
+    static void ShowMenu()
+    {
+        string menu = "";
+        menu += "Please select one of the following choices:\n";
+        menu += "1. Write\n";
+        menu += "2. Display\n";
+        menu += "3. Load\n";
+        menu += "4. Save\n";
+        menu += "5. Quit\n";
+        menu += "What would you like to do? ";
+
+        Console.Write(menu);
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        // Create a new journal object to work with.
+        Journal activeJournal = new Journal();
+
+        // Create other empty variables for use in loop
+        string userInput;
+        string filename;
+
+        // I want this to loop until the user chooses to stop it.
+        while (true)
+        {
+            ShowMenu();
+            userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "1":
+                    Entry newEntry = new Entry();
+                    // TODO: Go through steps to fill out entry
+                    activeJournal.AddEntry(newEntry);
+                    break;
+                case "2":
+                    activeJournal.DisplayAll();
+                    break;
+                case "3":
+                    // TODO: Get user input for filename
+                    filename = "";
+                    activeJournal.LoadFromFile(filename);
+                    break;
+                case "4":
+                    // TODO: Get user input for filename
+                    filename = "";
+                    activeJournal.SaveToFile(filename);
+                    break;
+                case "5":
+                    // Return in the main program quits it.
+                    return;
+                default:
+                    Console.WriteLine($"{userInput} is not a valid option.");
+                    break;
+            }
+
+
+        }
     }
 }
