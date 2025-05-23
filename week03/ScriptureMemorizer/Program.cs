522a30dfdffe813fa9366bf9634d3a28d0266efa
Zach Barnett <zacharybarnett96@gmail.com>
2025-05-22 22:16:48 -0700

    Working Program

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,12 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        // Hard-coded references.  Above and beyond could be getting these from user input.
+        string reference = "3rd Nephi 11:4-7";
+        string verse = "And it came to pass that again they heard the voice, and they understood it ";
+        verse += "not. And again the third time they did hear the voice, and did open their ears to ";
+        verse += "hear it; and their eyes were towards the sound thereof; and they did look ";
+        verse += "steadfastly towards heaven, from whence the sound came. And behold, the third ";
+        verse += "time they did understand the voice which they heard; and it said unto them: Behold ";
+        verse += "my Beloved Son, in whom I am well pleased, in whom I have glorified my name—hear ye him.";
 
-        Reference ref1 = new Reference("John", 3, 16);
-        Console.WriteLine(ref1.GetDisplayText());
+        int numberToHide = 3;
 
-        Reference ref2 = new Reference("Mosiah", 7, 4, 7);
-        Console.WriteLine(ref2.GetDisplayText());
+        Scripture script1 = new Scripture(reference, verse);
+
+        while (!script1.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(script1.GetDisplayText());
+            Console.WriteLine();
+            Console.Write("Press enter to continue or type 'quit' to finish:");
+
+            string userInput = Console.ReadLine();
+
+            if (userInput.ToLower() == "quit")
+            {
+                // Leave the main function, ending the program;
+                return;
+            }
+            // Else continue
+            script1.HideRandomWords(numberToHide);
+
+            // Loop back to the top
+            // Console.WriteLine(script1.GetDisplayText());
+        }
+
+        // Once all is hidden, print the scripture one more time with all the blanks
+        Console.Clear();
+        Console.WriteLine(script1.GetDisplayText());
     }
 }
--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,30 +1,108 @@
 using System;
 using System.Collections.Concurrent;
+using System.Threading.Tasks.Dataflow;
 
 public class Scripture
 {
     private Reference _reference;
     private List<Word> _words;
 
-    public Scripture(Reference reference, string text)
+    public Scripture(string reference, string text)
     {
-        //Parse reference into book, chapter, verse, and optional ending verse
+        // Parse reference into book, chapter, verse, and optional ending verse
+        string[] parts = reference.Split([' ', ':', '-']);
+        // Words Of Mormon 1:1-3 => ["Words", "Of", "Mormon", "1", "1", "3"]
+        // This is the most complicated reference, a multi-word book, and multiple verses
 
-        //Split the text into separate words and add Word class instances to the list
+        // If there is a hyphen, use the 4-parameter constructor
+        if (reference.IndexOf('-') >= 0)
+        {
+            string[] bookParts = parts[0..^3];
+            string book = string.Join(" ", bookParts);
+            int chapter = int.Parse(parts[^3]);
+            int verse = int.Parse(parts[^2]);
+            int endVerse = int.Parse(parts[^1]);
+
+            _reference = new Reference(book, chapter, verse, endVerse);
+        }
+        else // Use the 3-parameter constructor
+        {
+            string[] bookParts = parts[0..^2];
+            string book = string.Join(" ", bookParts);
+            int chapter = int.Parse(parts[^2]);
+            int verse = int.Parse(parts[^1]);
+
+            _reference = new Reference(book, chapter, verse);
+        }
+
+        // Split the text into separate words and add Word class instances to the list
+        // Initialize the list
+        _words = new List<Word>();
+
+        string[] verseText = text.Split();
+        foreach (string verseWord in verseText)
+        {
+            Word newWord = new Word(verseWord);
+            _words.Add(newWord);
+        }
     }
 
     public void HideRandomWords(int numberToHide)
     {
-        return;
+        Random random = new Random();
+        int counter = 0;
+        int wordsLeftThisRound;
+        int numAvailableWords = 0;
+
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                numAvailableWords++;
+            }
+        }
+
+        if (numAvailableWords < numberToHide)
+        {
+            wordsLeftThisRound = numAvailableWords;
+        }
+        else
+        {
+            wordsLeftThisRound = numberToHide;
+        }
+
+        while (counter < wordsLeftThisRound)
+            {
+                int i = random.Next(_words.Count);
+                if (!_words[i].IsHidden())
+                {
+                    counter++;
+                    _words[i].Hide();
+                }
+            }
     }
 
     public string GetDisplayText()
     {
-        return "";
+        List<string> parts = [$"{_reference.GetDisplayText()}"];
+
+        foreach (Word verseWord in _words)
+        {
+            parts.Add(verseWord.GetDisplayText());
+        }
+        return string.Join(' ', parts);
     }
 
     public bool IsCompletelyHidden()
     {
-        return false;
+        // If any one word is not hidden, this is false
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
