2850c7acff0c75462dc05aa642fab9aab57f7f98
Zach Barnett <zacharybarnett96@gmail.com>
2025-06-11 20:09:30 -0700

    Working Breathing Activity

--- week05/Mindfulness/BreatingActivity.cs
+++ week05/Mindfulness/BreatingActivity.cs
@@ -2,20 +2,41 @@ public class BreathingActivity : Activity
 {
     //Variables
     //Constructors
-    public BreathingActivity() : base()
+    public BreathingActivity()
     {
-        //No variables.  Everything gets handled by the base constructor
+        //No base constructor since the base class variables are set specific to the class used.
+        _name = "Breathing Activity";
+        _description = "This activity will help you relax by walking you through breathing " +
+                       "in and out slowly.  Clear your mind and focus on your breathing.";
+
+        //Duration does not need to be set since it gets set as part of the welcome message.
     }
 
-    public BreathingActivity(string name, string description, int duration) : base(name, description, duration)
+    public BreathingActivity(string name, string description) : base(name, description)
     {
         //No variables.  Everything gets handled by the base constructor
     }
 
     public void Run()
     {
-        DisplayStartingMessage();        
+        DisplayStartingMessage();
+
         //Run the activity.
+        Console.Write("Breathe in  ");
+        ShowCountDownSeconds(2);
+        Console.Write("Now breathe out  ");
+        ShowCountDownSeconds(3);
+        Console.WriteLine();
+
+        for (int i = _duration; i > 0; i -= 10)
+        {
+            Console.Write("Breathe in  ");
+        ShowCountDownSeconds(4);
+        Console.Write("Now breathe out  ");
+        ShowCountDownSeconds(6);
+        Console.WriteLine();
+        }
+
         DisplayEndingMessage();
     }
 }
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,4 +1,10 @@
 using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+//Above and Beyond - Input parsing.  While it's important to not belittle someone's attempts to be mindful,
+//there are prompts that need a valid input to prevent the program from breaking.  Specifically the menu
+//selection and the duration entry.  In these cases, validation is used to ensure a valid entry is provided.
 
 class Program
 {
@@ -8,5 +14,47 @@ class Program
         Activity testAct = new();
         testAct.ShowSpinner(5);
         testAct.ShowCountDownSeconds(5);
+
+        BreathingActivity breathingActivity = new BreathingActivity();
+
+        string listingActName = "Breathing Activity";
+        string listingActDescription = "This activity will help you relax by walking you through breathing "+
+                                         "in and out slowly.  Clear your mind and focus on your breathing.";
+        ListingActivity listingActivity = new ListingActivity(listingActName, listingActDescription);
+
+        ReflectingActivity reflectingActivity = new ReflectingActivity();
+
+        string userInput = "";
+
+        while (userInput != "4")
+        {
+            //Draw menu
+            Console.Clear();
+            Console.WriteLine("Menu Options:\n" +
+                          "  1. Start breathing activity\n" +
+                          "  2. Start reflecting activity\n" +
+                          "  3. Start listing activity\n" +
+                          "  4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "1":
+                    breathingActivity.Run();
+                    break;
+                case "2":
+                    reflectingActivity.Run();
+                    break;
+                case "3":
+                    listingActivity.Run();
+                    break;
+                case "4":
+                    return;
+                default:
+                    Console.WriteLine($"\"{userInput}\" is not a valid menu option.  Please try again.");
+                    break;
+            }
+        }
     }
 }
